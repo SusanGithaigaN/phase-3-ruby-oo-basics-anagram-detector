cecfdd5da9a1b891c3d6792db74f550e672fc566
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-09 04:32:19 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+    #  throw the argument
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(condition)
+        # loop through the array 
+        condition.select do |w|
+            # split the word
+            # sort the words
+            # do comparison
+            w.split("").sort == word.split("").sort
+        end
+    end
+
+end    
